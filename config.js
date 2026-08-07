d7829f75d00594fe3ec17acc920e65bbe22bdd90
wlstjd88-sys <wlstjd88@gmail.com>
2026-08-07 16:14:57 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+
+    // 개발 모드 여부
+    DEV_MODE: true,
+
+    // Python 서버 주소
+    API_BASE_URL: "http://127.0.0.1:5000",
+
+    // 향후 OpenAI API 사용 여부
+    USE_OPENAI: false,
+
+    // 버전
+    VERSION: "0.2.0"
+
+};
